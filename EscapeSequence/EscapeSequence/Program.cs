2a169699710364dedd95d37624fc2acf73f521e3
Trevor Hanson <52029257+trucetht@users.noreply.github.com>
2020-07-02 15:45:02 -0400

    Add files via upload

---
+++ EscapeSequence/EscapeSequence/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EscapeSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // escape sequence for quotation marks
+            Console.WriteLine("\"C#\" is really cool!");
+
+            // escape sequence for single quotes
+            Console.WriteLine("\'C#\' is really cool!");
+
+            // escape sequence for a new line
+            Console.WriteLine("C#\nis\nreally\ncool!");
+
+            // escape sequence for an alert sound
+            Console.WriteLine("\aC# is really cool!");
+        }
+    }
+}
